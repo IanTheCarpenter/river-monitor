213eee81f00f434f37be5801f5a3ee99446b204e
Ian <ian@carpenter.cx>
2025-05-12 10:06:48 -0500

    moved river schemas to separate folder

--- main.go
+++ main.go
@@ -45,20 +45,3 @@ func main() {
 	fmt.Println(result.DataCollectionSites[0].Latitude)
 
 }
-
-type RiverSchema struct {
-	RiverName           string               `bson:"river_name"`
-	DataCollectionSites []DataCollectionSite `bson:"data_collection_sites"`
-}
-
-type DataCollectionSite struct {
-	URL        string     `bson:"url"`
-	Latitude   float64    `bson:"latitude"`
-	Longitude  float64    `bson:"longitude"`
-	Thresholds Thresholds `bson:"thresholds"`
-}
-
-type Thresholds struct {
-	High int64 `bson:"high"`
-	Low  int64 `bson:"low"`
-}
---
+++ schemas/riverSchema.go
@@ -0,0 +1,16 @@
+type RiverSchema struct {
+	RiverName           string               `bson:"river_name"`
+	DataCollectionSites []DataCollectionSite `bson:"data_collection_sites"`
+}
+
+type DataCollectionSite struct {
+	URL        string     `bson:"url"`
+	Latitude   float64    `bson:"latitude"`
+	Longitude  float64    `bson:"longitude"`
+	Thresholds Thresholds `bson:"thresholds"`
+}
+
+type Thresholds struct {
+	High int64 `bson:"high"`
+	Low  int64 `bson:"low"`
+}
