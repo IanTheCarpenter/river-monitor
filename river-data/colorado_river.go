ee5728201e24bdf79873b88c4b84a19b5c543cdc
Ian <ian@carpenter.cx>
2025-05-17 13:26:14 -0500

    separated schemas and riverdata to the root dir

---
+++ river-data/colorado_river.go
@@ -0,0 +1,29 @@
+package river_data
+
+import "github.com/IanTheCarpenter/river-monitor/schemas"
+
+var coloradoRiver = schemas.River{
+	RiverName: "colorado",
+	PointsOfInterest: []schemas.PointOfInterest{
+		{
+			Name:       "Little Webberville Park",
+			Type:       "Public Access",
+			Latitude:   30.22946835858506,
+			Longitude:  -97.51890756915127,
+			Indicators: []string{"lcra_5423"},
+		},
+	},
+	DataCollectionSites: []schemas.DataCollectionSite{
+		{
+			Name:      "lcra_5423",
+			Agency:    "lcra",
+			URL:       "https://hydromet.lcra.org/api/GetDataBySite/5423/flow",
+			Latitude:  30.22946835858506,
+			Longitude: -97.51890756915127,
+			Thresholds: schemas.Thresholds{
+				High: 12,
+				Low:  7.25,
+			},
+		},
+	},
+}
---
+++ schemas/go.mod
@@ -0,0 +1,5 @@
+module github.com/IanTheCarpenter/river-monitor/schemas
+
+go 1.23.5
+
+require go.mongodb.org/mongo-driver/v2 v2.2.1
